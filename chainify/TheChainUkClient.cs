8a61c0fb00908a9e67c89ba20b33f2c242e5c1e6
Kunal Buxani <kunalbuxani@msn.com>
2019-10-31 14:31:39 +0000

    Adding integration test for RSS feed client

---
+++ chainify/TheChainUkClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Chainify
+{
+    public class TheChainUkClient
+    {
+        public TheChainUkClient()
+        {
+            
+        }
+
+        public async Task<XDocument> GetFeed() =>
+            await XDocument.LoadAsync(await new HttpClient().GetStreamAsync("https://www.thechain.uk/feed/"),
+                LoadOptions.None, CancellationToken.None);
+    }
+}
